edd5ea69c2893a44c9c3c555d0a285f97d5bc707
hhbngo <58571870+hhbngo@users.noreply.github.com>
2020-04-05 03:09:55 -0500

    Update script.js

--- portfolio/script.js
+++ portfolio/script.js
@@ -2,3 +2,5 @@ function onClickMenu() {
   document.getElementById("menu").classList.toggle("change");
   document.getElementById("ham-nav").classList.toggle("change");
 }
+
+$('body').hasClass('no-touch') ? event = 'mouseenter mouseleave' : event = 'click';

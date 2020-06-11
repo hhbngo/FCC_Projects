e15b8c28759606f0505f8d6f0d7ac76f73e35ca3
hhbngo <58571870+hhbngo@users.noreply.github.com>
2020-06-11 08:49:20 -0500

    Update app.js

--- personal/app.js
+++ personal/app.js
@@ -62,23 +62,11 @@ var UIcontroller = (function () {
     bar2: ".bar--2",
     bar3: ".bar--3",
   };
-
+  
   var formatNumber = function (num, type) {
-    var numSplit, int, dec, type;
-    num = Math.abs(num);
-    num = num.toFixed(2);
-
-    numSplit = num.split(".");
-    int = numSplit[0];
-
-    if (int.length > 3) {
-      int = int.substr(0, int.length - 3) + "," + int.substr(int.length - 3, 3);
-    }
-
-    dec = numSplit[1];
-
-    return (type === "dec" ? "-" : "+") + "$" + int + "." + dec;
+    return (type === "dec" ? "- " : "+ ") + num.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
   };
+
 
   return {
     getDOMstrings: function () {
